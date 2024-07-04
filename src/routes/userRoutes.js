7b280c41de0ee28e4c29f85641179985b88efa0e
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-07-04 16:51:28 -0300

    aaaa

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import UserController from "../controllers/userController";
+import UserController from "../controllers/userController.js";
 
 export const router = express.Router();
 
